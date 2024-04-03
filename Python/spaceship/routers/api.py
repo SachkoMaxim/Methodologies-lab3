e9e392450eddb5cd1d07c487bba628bfb85f524d
Maxim Sachko <114748761+SachkoMaxim@users.noreply.github.com>
2024-04-03 10:55:40 +0300

    added matrix product endpoint Switched image back to python:3.9-slim for the future comparison with alpine. Implemented an endpoint for api which returns a product of two random 10x10 matrices, with all the necessary dependencies.

--- Python/spaceship/routers/api.py
+++ Python/spaceship/routers/api.py
@@ -1,7 +1,22 @@
+import numpy as np
 from fastapi import APIRouter
 
 router = APIRouter()
 
+@router.get('/matrix')
+def matrix_product() -> dict:
+    matrix_a = np.random.rand(10, 10)
+    matrix_b = np.random.rand(10, 10)
+
+    product = np.dot(matrix_a, matrix_b)
+
+    result = {
+        "matrix_a": matrix_a.tolist(),
+        "matrix_b": matrix_b.tolist(),
+        "product": product.tolist()
+    }
+
+    return result
 
 @router.get('')
 def hello_world() -> dict:
